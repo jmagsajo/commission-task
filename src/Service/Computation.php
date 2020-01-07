6fe107b668d803a6c1f460691cfd97d08a100e06
Joseph Magsajo <jmagsajo@yondu.com>
2020-01-07 14:07:45 +0800

    update

--- src/Service/Computation.php
+++ src/Service/Computation.php
@@ -22,16 +22,11 @@ class Computation
 
     public function convertCurrency($currency, $commission_fee): float
     {
-        if($currency == "USD")
-        {
+        if ($currency == "USD") {
             return (float) $this->math->mul( strval($commission_fee), strval($this->usd));
-        }
-        else if($currency == "JPY")
-        {
+        } elseif ($currency == "JPY") {
             return (float) $this->math->mul( strval($commission_fee), strval($this->jpy) );
-        }
-        else
-        {
+        } else {
             return (float) $commission_fee;
         }
     }
@@ -46,35 +41,30 @@ class Computation
             $cash = $data[4];
             $currency = $data[5];
 
-            if($currency == "JPY")
-            {
-                $cash = $this->math->div(strval($cash), strval($this->jpy));
+            if ($currency == "JPY") {
+                $cash = $this->math->div(strval($cash), strval($this->jpy)); //if the currency is jpy the cash will e converted to jpy
+            } elseif($currency == "USD") {
+                $cash = $this->math->div(strval($cash), strval($this->usd)); //if the currency is usd the cash will e converted to usd
             }
-            else if($currency == "USD")
-            {
-                $cash = $this->math->div(strval($cash), strval($this->usd));
-            }
-            
+            /* commission fee calculation*/
             $commission_fee = (float) $this->math->mul(strval($cash), strval($this->commission));
-            $commission_fee = round(ceil($commission_fee*100)/100, 2);
+            $commission_fee = round(ceil($commission_fee*100)/100, 2); //cents is always ceiled
 
             switch($data[3])
             {
                 case "cash_out" :
                     $person = $data[2];
                     if($person == "legal"){
-                        if($commission_fee > 0.50 )
-                        {
+                        if ($commission_fee > 0.50 ) { //legal person Commission fee - 0.3% from amount, but not less than 0.50 EUR for operation.
+                        
                             $commission_fee = $this->convertCurrency($currency, 0.50);
                             array_push($output, number_format( (float) $commission_fee, 2));
-                        }
-                        else
-                        {
+                        } else {
                             $commission_fee = $this->convertCurrency($currency, $commission_fee);
                             $total = number_format( (float) $commission_fee, 2);
                             array_push($output, $total);
                         }
-                    }else{
+                    } else { //natural person Commission fee - 0.3% from cash out amount.
                         
                         $commission_fee = $this->convertCurrency($currency, $commission_fee);
                         $total = number_format( (float) $commission_fee, 2);
@@ -84,13 +74,10 @@ class Computation
 
                 break;
                 case "cash_in" :
-                    if($commission_fee > 5 )
-                    {
+                    if ($commission_fee > 5 ) { //cash in Commission fee - 0.03% from total amount, but no more than 5.00 EUR.
                         $commission_fee = $this->convertCurrency($currency, 5.00);
                         array_push($output,  number_format( (float) $commission_fee, 2) );
-                    }
-                    else
-                    {
+                    } else {
                         $commission_fee = $this->convertCurrency($currency, $commission_fee);
                         $total = number_format( (float) $commission_fee, 2);
                         array_push($output, $total);
